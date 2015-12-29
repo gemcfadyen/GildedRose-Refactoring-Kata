ad89734812bd90d84fe30c80fbc1081e80cfed97
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-12-29 13:34:08 +0000

    Extracts standard item out into a class

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -5,7 +5,12 @@ class GildedRose {
     private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
     Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
 
     public void updateQuality() {
         for (Item currentItem : items) {
@@ -15,13 +20,9 @@ class GildedRose {
             } else if (isBackstagePasses(currentItem)) {
                 BackstagePassItem backstagePassItem = new BackstagePassItem();
                 backstagePassItem.process(currentItem);
-            } else if (notSulfuras(currentItem) && qualityIsGreaterThanZero(currentItem)) {
-                defaultQualityDecrease(currentItem);
-                defaultSellInDecrement(currentItem);
-
-                if (getSellInOf(currentItem) < 0) {
-                    defaultQualityDecrease(currentItem);
-                }
+            } else if (notSulfuras(currentItem)) {
+                StandardItem standardItem = new StandardItem();
+                standardItem.process(currentItem);
             }
         }
     }
@@ -30,28 +31,8 @@ class GildedRose {
         return getNameOf(currentItem).equals(AGED_BRIE);
     }
 
-    private void defaultQualityDecrease(Item currentItem) {
-        setItemQualityTo(currentItem, decrementQualityOfItem(currentItem));
-    }
-
-    private void defaultSellInDecrement(Item currentItem) {
-        setSellInOf(currentItem, decrementSellInOfItem(currentItem));
-    }
-
-    private void defaultQualityIncrease(Item currentItem) {
-        setItemQualityTo(currentItem, incrementQualityOfItem(currentItem));
-    }
-
-    private boolean sellInIsWithinTenDays(Item currentItem) {
-        return getSellInOf(currentItem) < 11;
-    }
-
     private boolean isBackstagePasses(Item currentItem) {
         return getNameOf(currentItem).equals(BACKSTAGE_PASSES);
-    }
-
-    private void setSellInOf(Item currentItem, int sellIn) {
-        currentItem.sellIn = sellIn;
     }
 
     private boolean notSulfuras(Item currentItem) {
@@ -62,36 +43,16 @@ class GildedRose {
         return getQualityOf(currentItem) > 0;
     }
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
-
     private String getNameOf(Item currentItem) {
         return currentItem.name;
-    }
-
-    private void setItemQualityTo(Item currentItem, int quality) {
-        currentItem.quality = quality;
     }
 
     private int getQualityOf(Item item) {
         return item.quality;
     }
 
-    private int decrementSellInOfItem(Item currentItem) {
-        return getSellInOf(currentItem) - 1;
-    }
-
     private int getSellInOf(Item item) {
         return item.sellIn;
-    }
-
-    private int incrementQualityOfItem(Item currentItem) {
-        return getQualityOf(currentItem) + 1;
-    }
-
-    private int decrementQualityOfItem(Item currentItem) {
-        return getQualityOf(currentItem) - 1;
     }
 
     private class AgedBrieItem {
@@ -100,9 +61,21 @@ class GildedRose {
             return getQualityOf(currentItem) < MAXIMUM_QUALITY;
         }
 
+        private int incrementQualityOfItem(Item currentItem) {
+            return getQualityOf(currentItem) + 1;
+        }
+
         private void qualityIncreasesAsSellInDecreases(Item currentItem) {
             defaultQualityIncrease(currentItem);
             defaultSellInDecrement(currentItem);
+        }
+
+        private void defaultQualityIncrease(Item currentItem) {
+            setItemQualityTo(currentItem, incrementQualityOfItem(currentItem));
+        }
+
+        private void setItemQualityTo(Item currentItem, int quality) {
+            currentItem.quality = quality;
         }
 
         private void brieIncreasesQualityWithAge(Item currentItem) {
@@ -121,6 +94,19 @@ class GildedRose {
         private int getQualityOf(Item item) {
             return item.quality;
         }
+
+        private void defaultSellInDecrement(Item currentItem) {
+            setSellInOf(currentItem, decrementSellInOfItem(currentItem));
+        }
+
+        private void setSellInOf(Item currentItem, int sellIn) {
+            currentItem.sellIn = sellIn;
+        }
+
+        private int decrementSellInOfItem(Item currentItem) {
+            return getSellInOf(currentItem) - 1;
+        }
+
     }
 
     private class BackstagePassItem {
@@ -128,6 +114,34 @@ class GildedRose {
         private void qualityIncreasesAsSellInDecreases(Item currentItem) {
             defaultQualityIncrease(currentItem);
             defaultSellInDecrement(currentItem);
+        }
+
+        private int incrementQualityOfItem(Item currentItem) {
+            return getQualityOf(currentItem) + 1;
+        }
+
+        private int decrementSellInOfItem(Item currentItem) {
+            return getSellInOf(currentItem) - 1;
+        }
+
+        private boolean sellInIsWithinTenDays(Item currentItem) {
+            return getSellInOf(currentItem) < 11;
+        }
+
+        private void defaultQualityIncrease(Item currentItem) {
+            setItemQualityTo(currentItem, incrementQualityOfItem(currentItem));
+        }
+
+        private void setItemQualityTo(Item currentItem, int quality) {
+            currentItem.quality = quality;
+        }
+
+        private void defaultSellInDecrement(Item currentItem) {
+            setSellInOf(currentItem, decrementSellInOfItem(currentItem));
+        }
+
+        private void setSellInOf(Item currentItem, int sellIn) {
+            currentItem.sellIn = sellIn;
         }
 
         private void additionalQualitySetForBackstagePasses(Item currentItem) {
@@ -164,6 +178,48 @@ class GildedRose {
                 qualityIncreasesAsSellInDecreases(currentItem);
                 additionalQualitySetForBackstagePasses(currentItem);
                 backstagePassesLooseAllValueAfterSellIn(currentItem);
+            }
+        }
+    }
+
+    private class StandardItem {
+
+        private void defaultQualityDecrease(Item currentItem) {
+            setItemQualityTo(currentItem, decrementQualityOfItem(currentItem));
+        }
+
+        private int decrementQualityOfItem(Item currentItem) {
+            return getQualityOf(currentItem) - 1;
+        }
+
+        private int decrementSellInOfItem(Item currentItem) {
+            return getSellInOf(currentItem) - 1;
+        }
+
+        private void defaultSellInDecrement(Item currentItem) {
+            setSellInOf(currentItem, decrementSellInOfItem(currentItem));
+        }
+
+        private int getSellInOf(Item item) {
+            return item.sellIn;
+        }
+
+        private void setSellInOf(Item currentItem, int sellIn) {
+            currentItem.sellIn = sellIn;
+        }
+
+        private void setItemQualityTo(Item currentItem, int quality) {
+            currentItem.quality = quality;
+        }
+
+        private void process(Item currentItem) {
+            if (qualityIsGreaterThanZero(currentItem)) {
+                defaultQualityDecrease(currentItem);
+                defaultSellInDecrement(currentItem);
+
+                if (getSellInOf(currentItem) < 0) {
+                    defaultQualityDecrease(currentItem);
+                }
             }
         }
     }
